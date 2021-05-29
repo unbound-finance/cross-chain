b84fc1e21fa926fe82bbde1f77a1539c329acd55
Saurabh Bachawe <sb4514.work@gmail.com>
2021-05-29 14:49:33 +0530

    ft: deposit

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,3 +1,48 @@
 //SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.6;
 
-pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+/**
+ * @title Bridge
+ * @notice Base bridge contract, locks the funds and mints the equivalent (1:1 ratio)
+ * amount of tokens on another chain.
+ */
+
+contract Bridge {
+    using Address for address payable;
+    using SafeMath for uint256;
+
+    event Lock(address payee, address token, uint256 amount);
+    event Claim(address payee, address token, uint256 amount);
+
+    struct UserTokens {
+        address token;
+        mapping(address => uint256) locked;
+    }
+
+    mapping(address => UserTokens[]) public userTokens;
+
+    function getLockedBalance(address _token) public view returns (uint256) {
+        return userTokens[msg.sender][_token].locked;
+    }
+
+    function deposit(address _token, uint256 _amt) external {
+        // update balance of specified token
+        u = UserTokens({token: _token});
+        u.locked[msg.sender] = u.locked[msg.sender].add(_amt);
+
+        userTokens[msg.sender].push(u);
+
+        IERC20 token = IERC20(_token);
+        token.transferFrom(msg.sender, address(this), _amt);
+
+        emit Lock(msg.sender, _token, _amt);
+    }
+
+    function claim(uint256 _amt) external {
+        // should be called when tokens are locked
+    }
+}
